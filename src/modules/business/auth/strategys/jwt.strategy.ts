a62c402a87efafb7eec05a4dd392c9f81c118fcd
darian7 <yoj.cabrera@udla.edu.co>
2023-09-25 03:05:14 -0500

    fix(jwt.strategy): validate user

--- src/modules/business/auth/strategys/jwt.strategy.ts
+++ src/modules/business/auth/strategys/jwt.strategy.ts
@@ -1,16 +1,18 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 
 import authenticationConfig from '../../../../config/authentication-config/authentication.config';
 import { JwtPayload, RequestUserJwt } from '../interfaces/auth.interface';
+import { UsersService } from '../../users/services/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @Inject(authenticationConfig.KEY)
     private configApp: ConfigType<typeof authenticationConfig>,
+    private usersService: UsersService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -20,6 +22,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload): Promise<RequestUserJwt> {
+    let user;
+    try {
+      user = await this.usersService.getUserByEmail(payload.sub);
+    } catch (error) {}
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
     return { email: payload.sub, roles: payload.roles };
   }
 }
